df3a312d66b13731a7905b323305b49707866a1a
root <vnsmuthu@gmail.com>
2019-05-26 10:53:48 +0530

    Updates

---
+++ precache-manifest.b22e50663dae5ad57cae0681bd7aaa71.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da1558562965dd965567420a3e85fbe5",
+    "url": "/Angamuthu20.github.io/index.html"
+  },
+  {
+    "revision": "e3aac2d5bced07f7a4ee",
+    "url": "/Angamuthu20.github.io/static/js/2.66443dcb.chunk.js"
+  },
+  {
+    "revision": "415eaf354c2a926ce180",
+    "url": "/Angamuthu20.github.io/static/js/main.39313c2f.chunk.js"
+  },
+  {
+    "revision": "85da58a5f3ceb1e823ec",
+    "url": "/Angamuthu20.github.io/static/js/runtime~main.d54cff43.js"
+  }
+]);
